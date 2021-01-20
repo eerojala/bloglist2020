6a2ef5209a61f97adcfa3592b5694d38493fc771
eerojala <eero.ojala@helsinki.fi>
2021-01-20 14:08:10 +0200

    4.9 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,7 +2,7 @@ const { TestScheduler } = require('jest')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const api = supertest(app)
+const api = supertest(app) // app.js contains no code for actually starting the application, but supertest is able to start it in an internal port it uses
 const Blog = require('../models/blog')
 
 const initialBlogs = [
@@ -49,6 +49,24 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(initialBlogs.length)
 })
 
+test('all returned blog have field id instead of _id', async () => {
+  const response = await api.get('/api/blogs')
+
+  const ids = response.body.map(b => b.id)
+  const _ids = response.body.map(b => b._id)
+
+  expect(ids).toHaveLength(3)
+  expect(ids[0]).toBeDefined()
+  expect(ids[1]).toBeDefined()
+  expect(ids[2]).toBeDefined()
+  
+  expect(_ids).toHaveLength(3)
+  expect(_ids[0]).toBeUndefined()
+  expect(_ids[1]).toBeUndefined()
+  expect(_ids[2]).toBeUndefined()
+})
+
 afterAll(() => {
-  mongoose.connection.close()
+  mongoose.connection.close() // remember to close the database connection after all tests
+  // NOTE: if we run individual tests which do not utilize the database, the mongoose connection may remain open (because apparently in this case jest will not run the code in afterAll())
 })
